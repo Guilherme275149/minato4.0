7df8e9a9eb8f76ed4e2b5a74fa0e33ab06f1498b
Guilherme275149 <78052265+Guilherme275149@users.noreply.github.com>
2021-02-27 15:22:06 -0300

    Add files via upload

---
+++ dark/bash/nodejs13/antilag.js
@@ -0,0 +1,14 @@
+const antilag = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Minato*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Minato*
+
+⧿⧽ *Não se esqueça de se inscrever*
+⧿⧽ *Ative os sinos*
+⧿⧽ *Atenciosamente Minato*
+
+`
+}
+exports.antilag = antilag
---
+++ dark/bash/nodejs13/antitrace.js
@@ -0,0 +1,14 @@
+const antitrace = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Minato Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Minato*
+
+⧿⧽ *Não se esqueça de se inscrever*
+⧿⧽ *Ative os sinos*
+⧿⧽ *Atenciosamente Minato*
+
+`
+}
+exports.antitrace = antitrace
