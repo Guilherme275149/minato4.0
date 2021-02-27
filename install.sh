e88864554352c704efed0b3a9479ee5705a1ba69
Guilherme275149 <78052265+Guilherme275149@users.noreply.github.com>
2021-02-27 15:35:37 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+pkg upgrade
+cd
+git clone https://github.com/Guilherme275149/-minato4.0-
+clear
+cd ./-minato4.0-
+bash install.sh
+cd
+cd ./-minato4.0-
+npm start
+
+echo "[*] obrigado por utilizar o bot \"minatobot\" minato agradece"
